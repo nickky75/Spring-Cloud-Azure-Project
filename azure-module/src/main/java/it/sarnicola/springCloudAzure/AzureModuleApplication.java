2428f9c33aec2a44ffdefc57262e31c9cd3c8467
nickky75 <nicola.sarnicola@gmail.com>
2018-11-07 14:42:02 +0000

    Add generated project of spring cloud azure

---
+++ azure-module/src/main/java/it/sarnicola/springCloudAzure/AzureModuleApplication.java
@@ -0,0 +1,16 @@
+package it.sarnicola.springCloudAzure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AzureModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureModuleApplication.class, args);
+    }
+}
